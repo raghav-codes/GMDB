fe9b3605a1f45b60938afadb9cc94d7c4518e965
raghav-codes <raghavendra.balaji@gmail.com>
2021-03-20 12:00:53 -0400

    Refactored test case 1 GREEN

--- src/test/java/com/cognizant/GMDB/GMDBTestIT.java
+++ src/test/java/com/cognizant/GMDB/GMDBTestIT.java
@@ -1,18 +1,14 @@
 package com.cognizant.GMDB;
 
-import org.hamcrest.core.IsNull;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-//import static org.springframework.test.web.servlet.
-
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -26,7 +22,7 @@ public class GMDBTestIT {
 
         mockMvc.perform(get("/movies")).andExpect(status().isOk())
                 .andExpect(content().json("[ ]"));
-                //.andExpect(jsonPath("$.moviesName").value(IsNull.nullValue());
+
     }
 
 
