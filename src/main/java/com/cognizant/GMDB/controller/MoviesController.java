0c082242c14fe9a44e51147bea8de98c3164c343
j-tanz <john.tansor@cognizant.com>
2021-03-20 12:39:28 -0400

    completes test to post movie and check movie exists in list

--- src/main/java/com/cognizant/GMDB/controller/MoviesController.java
+++ src/main/java/com/cognizant/GMDB/controller/MoviesController.java
@@ -1,28 +1,33 @@
 package com.cognizant.GMDB.controller;
 
 import com.cognizant.GMDB.model.MoviesDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/movies")
 public class MoviesController {
 
+    List<MoviesDto> moviesRepository = new ArrayList<>();
+
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<MoviesDto> getMovies() {
-        return List.of();
+        return moviesRepository;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void addMovie() {
-
+    public void addMovie(@RequestBody MoviesDto movie) {
+        moviesRepository.add(movie);
     }
 }
--- src/test/java/com/cognizant/GMDB/GMDBTestIT.java
+++ src/test/java/com/cognizant/GMDB/GMDBTestIT.java
@@ -1,10 +1,12 @@
 package com.cognizant.GMDB;
 
 import com.cognizant.GMDB.model.MoviesDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -20,6 +22,9 @@ public class GMDBTestIT {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
     public void getMoviesEmptyTest() throws Exception {
         mockMvc.perform(get("/movies")).andExpect(status().isOk())
@@ -29,14 +34,16 @@ public class GMDBTestIT {
     @Test
     public void addMovieTest() throws Exception {
         MoviesDto movie = MoviesDto.builder().title("Citizen Kane").build();
-        mockMvc.perform(post("/movies"))
+        mockMvc.perform(post("/movies")
+                .content(objectMapper.writeValueAsString(movie))
+                .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated());
 
+        mockMvc.perform(get("/movies"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(1))
+                .andExpect(jsonPath("[0].title").value("Citizen Kane"));
     }
-
-
-
-
 
 
 }
